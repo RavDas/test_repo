bb5c4e7bfefa183757543806c258e38f32de3e6a
Raveen Dassanayake <raveend@99x.io>
2023-03-09 17:06:48 +0530

    uploaded scraper and run code

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 23 00:49:30 2023
+
+@author: RaveenD
+"""
+import glassdoor_scrapper as gs
+import pandas as pd
+
+path = "C:/Users/RaveenD/Documents/ds_salary_proj/chromedriver"
+df = gs.get_jobs("data scientists", 15, False, path, 15)
